7a0cd9d49d0c285318a8267f8fd2d37bd380feb0
Your Name <Academics@csrl.in>
2026-09-02 10:53:50 +0530

    Move Insights (Top Centres/Students) to top of Insights tab, above bar chart

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1291,9 +1291,7 @@ export default function AdminDashboard() {
           )}
         </div>
 
-      <div style={{ marginTop: 24 }}>
-        <InsightsDashboard data={data} overview={overview} topRanked={leaderboardTopRanked} bottomRanked={leaderboardBottomRanked} centreBoard={centreBoard} selectedTestKey={selectedLeaderboardTestKeys.length > 1 ? 'Multiple Tests' : (selectedLeaderboardTestKeys[0] || selectedTestKey)} onViewStudent={setViewingStudentId} />
-      </div>
+
     </div>
     );
   };
