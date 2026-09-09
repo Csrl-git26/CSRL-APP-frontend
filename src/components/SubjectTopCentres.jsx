d215ea631e3b2e3edce1ce69f7e0b44d3547d5fe
Your Name <Academics@csrl.in>
2026-09-09 12:38:48 +0530

    Set barGap to 0 so columns within a subject are connected

--- src/components/SubjectTopCentres.jsx
+++ src/components/SubjectTopCentres.jsx
@@ -75,7 +75,7 @@ export default function SubjectTopCentres({ centreBoard, onViewCentre }) {
       </div>
       <div style={{ flex: 1, minHeight: 180 }}>
         <ResponsiveContainer width="100%" height={180}>
-          <BarChart data={chartData} margin={{ top: 25, right: 5, left: -20, bottom: 0 }} barGap={4} barSize={26}>
+          <BarChart data={chartData} margin={{ top: 25, right: 5, left: -20, bottom: 0 }} barGap={0} barSize={26}>
             <defs>
               <linearGradient id="bar3DVertical" x1="0" y1="0" x2="1" y2="0">
                 <stop offset="0%" stopColor="#ffffff" stopOpacity={0.4} />
